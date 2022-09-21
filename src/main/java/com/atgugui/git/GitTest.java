950c816271881d0c7bdd93e3feebc36983225304
SevenZhao <yut_feng@163.com>
2022-09-21 17:59:28 +0800

    push commit

--- src/main/java/com/atgugui/git/GitTest.java
+++ src/main/java/com/atgugui/git/GitTest.java
@@ -15,6 +15,5 @@ public class GitTest {
         System.out.println("hello git-> hot-fix");
         System.out.println("hello git-> master test");
         System.out.println("hello git-> hot-fix test");
-        System.out.println("hello git-> push to github test");
     }
 }
