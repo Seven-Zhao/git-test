6b105ab7be9ea147789c0d06f2ea6c1dee539858
SevenZhao <yut_feng@163.com>
2022-09-21 17:07:05 +0800

    first commit

---
+++ src/main/java/com/atgugui/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atgugui.git;
+
+/**
+ * @author ：Seven Zhao
+ * @date ：Created in 2022/9/21 17:05
+ * @description： To - Do for git test
+ * @version: 1.0
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
